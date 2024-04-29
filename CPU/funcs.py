8db0d57b12c64c239f2e48778fa7ddc621ba1003
AnnNguy3n <nguyenhuuan.k64.hust@gmail.com>
2024-04-29 23:25:25 +0700

    .

--- CPU/funcs.py
+++ CPU/funcs.py
@@ -77,3 +77,56 @@ def multi_invest_2(WEIGHT,
             ValHarNgn2 = v
 
     return ValGeoNgn2, GeoNgn2, ValHarNgn2, HarNgn2
+
+
+@njit
+def maximum_min_3(WEIGHT,
+                  INDEX,
+                  PROFIT,
+                  INTEREST):
+    size = INDEX.shape[0] - 1
+    arr_loop = np.full((size-1)*5, -1.7976931348623157e+308, float)
+    for i in range(1, size):
+        start, end = INDEX[i], INDEX[i+1]
+        wgt_ = np.unique(WEIGHT[start:end])
+        wgt_[::-1].sort()
+        if len(wgt_) < 5:
+            arr_loop[5*(i-1):5*(i-1)+len(wgt_)] = wgt_
+        else:
+            arr_loop[5*(i-1):5*i] = wgt_[:5]
+
+    ValGeo3 = -1.0
+    MinGeo3 = -1.0
+    ValHar3 = -1.0
+    MinHar3 = -1.0
+    temp_profit = np.zeros(size-1)
+    for ii in range(len(arr_loop)):
+        v = arr_loop[ii]
+        bool_wgt = WEIGHT > v
+        temp_profit[:] = 0.0
+        for i in range(size-1, 0, -1):
+            start, end = INDEX[i], INDEX[i+1]
+            inv_cyc_val = bool_wgt[start:end]
+            lst_pro = PROFIT[start:end][inv_cyc_val]
+            if len(lst_pro) == 0:
+                temp_profit[i-1] = INTEREST
+            else:
+                temp_profit[i-1] = lst_pro.mean()
+
+        CurGeo = 1.7976931348623157e+308
+        CurHar = 1.7976931348623157e+308
+        for k in range(size-3):
+            tempGeo = geomean(temp_profit[k:k+3])
+            tempHar = harmean(temp_profit[k:k+3])
+            if tempGeo < CurGeo: CurGeo = tempGeo
+            if tempHar < CurHar: CurHar = tempHar
+
+        if CurGeo > MinGeo3:
+            ValGeo3 = v
+            MinGeo3 = CurGeo
+
+        if CurHar > MinHar3:
+            ValHar3 = v
+            MinHar3 = CurHar
+
+    return ValGeo3, MinGeo3, ValHar3, MinHar3
