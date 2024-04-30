d790a9d0d4c8ecaabd5f236f339a37c9bdff7ec3
AnnNguy3n <nguyenhuuan.k64.hust@gmail.com>
2024-04-30 10:38:25 +0700

    .

--- Methods/base.py
+++ Methods/base.py
@@ -158,3 +158,59 @@ def convert_strF_to_arrF(strF):
             idx += 1
 
     return arrF
+
+
+def similarity_filter(df_CT, fml_col, n=100, level=2):
+    list_CT = []
+    for ct in df_CT[fml_col]:
+        if type(ct) == str:
+            list_CT.append(convert_strF_to_arrF(ct))
+        else:
+            list_CT.append(ct)
+
+    list_index = _similarity_filter(list_CT, n, level)
+    return df_CT.iloc[list_index].reset_index(drop=True)
+
+
+@njit
+def check_similar_2(f1_, f2_, level):
+    f1 = np.unique(f1_[1::2])
+    f2 = np.unique(f2_[1::2])
+
+    if len(f1) > len(f2):
+        F1 = f1
+        F2 = f2
+    else:
+        F1 = f2
+        F2 = f1
+
+    count = 0
+    for i in F1:
+        if i not in F2:
+            count += 1
+
+    if count >= level:
+        return False
+
+    return True
+
+
+@njit
+def _similarity_filter(list_ct, num_CT, level):
+    list_index = [0]
+    count = 1
+    for i in range(1, len(list_ct)):
+        check = True
+        for j in list_index:
+            if check_similar_2(list_ct[i], list_ct[j], level):
+                check = False
+                break
+
+        if check:
+            list_index.append(i)
+            count += 1
+            if count == num_CT:
+                print(i)
+                break
+
+    return list_index
