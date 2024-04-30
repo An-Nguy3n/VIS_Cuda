66c3c5f1e4138dddef4d674767777708fd925c9e
AnnNguy3n <nguyenhuuan.k64.hust@gmail.com>
2024-04-30 11:14:09 +0700

    .

--- CPU/funcs.py
+++ CPU/funcs.py
@@ -130,3 +130,105 @@ def maximum_min_3(WEIGHT,
             MinHar3 = CurHar
 
     return ValGeo3, MinGeo3, ValHar3, MinHar3
+
+
+@njit
+def single_invest(WEIGHT,
+                  INDEX,
+                  PROFIT,
+                  PROFIT_RANK,
+                  PROFIT_RANK_NI,
+                  INTEREST):
+    size = INDEX.shape[0] - 1
+    arr_inv_idx = np.zeros(size, np.int64)
+    for i in range(size):
+        start, end = INDEX[i], INDEX[i+1]
+        wgt_ = WEIGHT[start:end]
+        arr_max = np.where(wgt_==max(wgt_))[0]
+        if arr_max.shape[0] == 1:
+            arr_inv_idx[i] = start + arr_max[0]
+        else:
+            arr_inv_idx[i] = -1
+
+    """
+    GeoMax, HarMax, ValGeo, GeoLim, ValHar, HarLim, GeoRank, HarRank
+    """
+
+    arr_profit = PROFIT[arr_inv_idx]
+    arr_profit[arr_inv_idx==-1] = INTEREST
+    GeoMax = geomean(arr_profit[1:])
+    HarMax = harmean(arr_profit[1:])
+
+    GeoLim = GeoMax
+    HarLim = HarMax
+    arr_inv_val = WEIGHT[arr_inv_idx]
+    arr_inv_val[arr_inv_idx==-1] = 1.7976931348623157e+308
+    arr_loop = arr_inv_val[1:]
+    ValGeo = min(arr_loop)
+    delta = max(1e-6*abs(ValGeo), 1e-6)
+    ValGeo -= delta
+    ValHar = ValGeo
+
+    for v in arr_loop:
+        temp_profit = np.where(arr_inv_val > v, arr_profit, INTEREST)
+        geo = geomean(temp_profit[1:])
+        har = harmean(temp_profit[1:])
+        if geo > GeoLim:
+            GeoLim = geo
+            ValGeo = v
+
+        if har > HarLim:
+            HarLim = har
+            ValHar = v
+
+    arr_rank = PROFIT_RANK[arr_inv_idx]
+    arr_rank = np.where(arr_inv_idx==-1, PROFIT_RANK_NI, arr_rank)
+    GeoRank = geomean(arr_rank[1:])
+    HarRank = harmean(arr_rank[1:])
+
+    return GeoMax, HarMax, ValGeo, GeoLim, ValHar, HarLim, GeoRank, HarRank
+
+
+@njit
+def multi_invest_1(WEIGHT,
+                   INDEX,
+                   PROFIT,
+                   INTEREST):
+    size = INDEX.shape[0] - 1
+    arr_loop = np.full((size-1)*5, -1.7976931348623157e+308, float)
+    for i in range(1, size):
+        start, end = INDEX[i], INDEX[i+1]
+        wgt_ = np.unique(WEIGHT[start:end])
+        wgt_[::-1].sort()
+        if len(wgt_) < 5:
+            arr_loop[5*(i-1):5*(i-1)+len(wgt_)] = wgt_
+        else:
+            arr_loop[5*(i-1):5*i] = wgt_[:5]
+
+    ValGeoNgn = -1.0
+    GeoNgn = -1.0
+    ValHarNgn = -1.0
+    HarNgn = -1.0
+    temp_profit = np.zeros(size-1)
+    for ii in range(len(arr_loop)):
+        v = arr_loop[ii]
+        bool_wgt = WEIGHT > v
+        temp_profit[:] = 0.0
+        for i in range(1, size):
+            start, end = INDEX[i], INDEX[i+1]
+            if np.count_nonzero(bool_wgt[start:end]) == 0:
+                temp_profit[i-1] = INTEREST
+            else:
+                temp_profit[i-1] = PROFIT[start:end][bool_wgt[start:end]].mean()
+
+        geo = geomean(temp_profit)
+        har = harmean(temp_profit)
+        if geo > GeoNgn:
+            GeoNgn = geo
+            ValGeoNgn = v
+
+        if har > HarNgn:
+            HarNgn = har
+            ValHarNgn = v
+
+    return ValGeoNgn, GeoNgn, ValHarNgn, HarNgn
